0eac04dd849490f62606124631223b9b72921ebc
Artur Takoev <artur.takoev@gmail.com>
2018-03-21 13:25:32 +0300

    After the course

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function () {
+    return [
+        { title: 'JS: The good parts', pages: 12 },
+        { title: 'Harry Potter', pages: 90 },
+        { title: 'The Lord of The Rings', pages: 123 },
+        { title: 'The good the bad and the queen', pages: 32 },
+        { title: 'Romeo and Julliet', pages: 55 }
+    ];
+};
